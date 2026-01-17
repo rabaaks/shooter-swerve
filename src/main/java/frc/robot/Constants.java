45d577a10ee81b790eaad799680d05d2203c2d8b
Blasty25 <sohamshah210@gmail.com>
2026-01-17 12:54:57 -0500

    BAH

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -7,13 +7,18 @@
 
 package frc.robot;
 
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.wpilibj.RobotBase;
+import frc.robot.subsystems.vision.VisionConstants;
 
 /**
- * This class defines the runtime mode used by AdvantageKit. The mode is always "real" when running
- * on a roboRIO. Change the value of "simMode" to switch between "sim" (physics sim) and "replay"
+ * This class defines the runtime mode used by AdvantageKit. The mode is always
+ * "real" when running
+ * on a roboRIO. Change the value of "simMode" to switch between "sim" (physics
+ * sim) and "replay"
  * (log replay from a file).
  */
 public final class Constants {
@@ -21,11 +26,14 @@ public final class Constants {
   public static final Mode currentMode = RobotBase.isReal() ? Mode.REAL : simMode;
 
   public static final Translation3d targetTranslation = new Translation3d(5, 5, 5);
-  public static final Translation2d targetFieldTranslation =
-      new Translation2d(targetTranslation.getX(), targetTranslation.getY());
+  public static final Translation2d targetFieldTranslation = new Translation2d(targetTranslation.getX(),
+      targetTranslation.getY());
   public static final double targetZ = targetTranslation.getZ();
 
   public static final boolean tuningMode = true;
+
+  public static Pose2d hubCenter = new Pose2d(VisionConstants.fieldLayout.getTagPose(18).get().getX(),
+      VisionConstants.fieldLayout.getTagPose(26).get().getY(), new Rotation2d());
 
   public static enum Mode {
     /** Running on a real robot. */
--- src/main/java/frc/robot/subsystems/shooter/ShooterIOSim.java
+++ src/main/java/frc/robot/subsystems/shooter/ShooterIOSim.java
@@ -1,6 +1,10 @@
 package frc.robot.subsystems.shooter;
 
+import edu.wpi.first.wpilibj.simulation.FlywheelSim;
+
 public class ShooterIOSim implements ShooterIO {
+    private FlywheelSim sim;
+     
 
     @Override
     public void updateInputs(ShooterIOInputs inputs) {
